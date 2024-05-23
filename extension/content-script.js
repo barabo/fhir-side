2541d37ae05300693705dde91b36ee0dd7faa3b0
Carl Anderson <barabo@users.noreply.github.com>
2024-05-23 10:49:26 -0500

    fix: re-send resource when regaining focus

--- extension/content-script.js
+++ extension/content-script.js
@@ -2,7 +2,7 @@
  * This content-script runs in the context of the page being viewed.  If the page is a FHIR
  * resource, it sends the raw resource content to the extension.  That's it!
  */
-window.onload = function() {
+function sendToExtension() {
   if (document.contentType === 'application/fhir+json') {
     chrome.runtime.sendMessage({
       action: 'receiveJsonResource',
@@ -11,4 +11,8 @@ window.onload = function() {
   } else {
     console.debug('Not a known FHIR resource contentType:', document.contentType);
   }
-};
+}
+
+// Send the resource to the extension when the page loads or regains focus.
+window.addEventListener('load', sendToExtension, false);
+window.addEventListener('focus', sendToExtension, false);
